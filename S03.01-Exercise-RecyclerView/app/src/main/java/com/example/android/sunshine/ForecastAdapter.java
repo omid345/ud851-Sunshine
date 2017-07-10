f07c01be12aa977dc13e0e1239d108d3224a2e5a
Omid Rajabi <omid.345@gmail.com>
2017-07-10 15:54:42 -0400

    completed S03, total 2 exercises

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,69 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by omid3 on 7/9/2017.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+    private String[] mWeatherData;
+
+    public ForecastAdapter() {
+
+    }
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view) {
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+        }
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.forecast_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+        String weatherForThisDay = mWeatherData[position];
+        holder.mWeatherTextView.setText(weatherForThisDay);
+    }
+
+    /**
+     * This method simply returns the number of items to display.
+     *
+     * @return The number of items available in our forecast.
+     */
+    @Override
+    public int getItemCount() {
+        if (mWeatherData == null) return 0;
+        return mWeatherData.length;
+    }
+
+    /**
+     * This method used to set the weather data on an already created forecast adapter.
+     *
+     * @param weatherData The new weather data to be displayed.
+     */
+    public void setWeatherData(String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+}
